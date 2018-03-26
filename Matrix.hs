e07ea75583c4b0c1eb05b3e48bd7368215fb90e8
schroffl <schroffl@users.noreply.github.com>
2018-03-26 16:05:35 +0200

    Add Matrix module
    This probably is neither performant nor a nice api, but I couldn't find anything fitting on hackage. This has to do for now.

---
+++ Matrix.hs
@@ -0,0 +1,51 @@
+module Matrix where
+
+import Data.Vector.Unboxed (Vector)
+import qualified Data.Vector.Unboxed as V
+import Data.List.Split (chunksOf)
+import Data.List(intercalate)
+
+newtype Matrix = Matrix (Vector Float)
+
+instance Show Matrix where
+  show (Matrix mat) = concat . show' 1 $ V.toList mat
+    where
+      show' n [] = []
+      show' n (x:xs)
+        | n `mod` 4 == 0 = (show x ++ "\n") : show' (n + 1) xs
+        | otherwise = (show x ++ " | ") : show' (n + 1) xs
+
+identity :: Matrix
+identity = Matrix . V.fromList $
+  [ 1, 0, 0, 0
+  , 0, 1, 0, 0
+  , 0, 0, 1, 0
+  , 0, 0, 0, 1
+  ]
+
+perspective :: Float -> Float -> Float -> Float -> Matrix
+perspective fovy aspect near far = Matrix . V.fromList $
+  [ f / aspect, 0,                   0,  0
+  ,          0, f,                   0,  0
+  ,          0, 0,   (far + near) * nf, -1
+  ,          0, 0, 2 * far * near * nf,  0
+  ]
+  where
+    f = 1 / tan (fovy / 2)
+    nf = 1 / (near - far)
+
+translate :: Float -> Float -> Float -> Matrix -> Matrix
+translate x y z (Matrix mat) = Matrix . V.fromList $
+  [ a00, a01, a02, a03
+  , a10, a11, a12, a13
+  , a20, a21, a22, a23
+  , a00 * x + a01 * y + a02 * z + a30
+  , a10 * x + a11 * y + a12 * z + a31
+  , a20 * x + a21 * y + a22 * z + a32
+  , a30 * x + a31 * y + a32 * z + a33
+  ]
+  where
+    (a00, a01, a02, a03) = (mat V.!  0, mat V.!  1, mat V.!  2, mat V.!  3)
+    (a10, a11, a12, a13) = (mat V.!  4, mat V.!  5, mat V.!  6, mat V.!  7)
+    (a20, a21, a22, a23) = (mat V.!  8, mat V.!  9, mat V.! 10, mat V.! 11)
+    (a30, a31, a32, a33) = (mat V.! 12, mat V.! 13, mat V.! 14, mat V.! 15)
